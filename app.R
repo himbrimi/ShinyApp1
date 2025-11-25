d1a8d7bc8736f5943e7c43079f554d7becf162d6
himbrimi <lomur00@gmail.com>
2025-11-25 17:14:38 +0100

    Created a simple shiny app that prints a line of text

---
+++ app.R
@@ -0,0 +1,24 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  "Hello, world!"
+)
+# Define server logic 
+server <- function(input, output, session){
+  
+}
+    
+
+
+# Run the application 
+shinyApp(ui, server)
